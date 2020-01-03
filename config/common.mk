c26a734b7a5aea14058dfb757bae0fa8dd771fe0
MSe1969 <mse1969@posteo.de>
2020-01-03 20:32:49 +0100

    oneplus3: Add device-specific overlay for changelog
    To avoid having to fork the device repo just for the Updater changelog
    
    Change-Id: I4308447f60d55fbe035351fc58aaecc2188bb1be

--- config/common.mk
+++ config/common.mk
@@ -9,9 +9,11 @@ PRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey
 PRODUCT_EXTRA_RECOVERY_KEYS := user-keys/releasekey
 endif
 
-# Updater URI
+# Updater URI and changelog
 PRODUCT_PROPERTY_OVERRIDES += \
     lineage.updater.uri=https://raw.githubusercontent.com/lin16-microG/OTA/lin-16.0-microG/$(LINEAGE_BUILD).json
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/lineage/overlay/changelog/$(LINEAGE_BUILD)
 
 # microG packages
 PRODUCT_PACKAGES += \
