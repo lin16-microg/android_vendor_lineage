c3d8542f297387d1aca26ee71d231c712878a9e9
MSe <mse1969@posteo.de>
2022-09-11 15:12:39 +0200

    common.mk: Remove Maps API v1
    Ref.: https://github.com/lineageos4microg/android_vendor_partner_gms
    
    Change-Id: I6b06c66a64ca1246bc72851ef37232d4be7399e0

--- config/common.mk
+++ config/common.mk
@@ -21,7 +21,6 @@ PRODUCT_PACKAGES += \
     FDroidPrivilegedExtension \
     IchnaeaNlpBackend \
     NominatimNlpBackend \
-    com.google.android.maps.jar \
     additional_repos.xml
 
 # Privacy-friendly netmonitor
