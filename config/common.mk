c43403dfc0e767f96d6ffab32ca676b6d6c2d572
MSe <mse1969@posteo.de>
2023-06-18 22:42:55 +0200

    microG: Remove location backend modules
    microG now uses location services internally and has dropped its
    support for modules, due to too aggressive 'energy saving' modes
    in recent android releases.
    
    Change-Id: I0e7cff737fb931340d604f44d505caefd3f50fc7

--- config/common.mk
+++ config/common.mk
@@ -19,8 +19,6 @@ PRODUCT_PACKAGES += \
     FakeStore \
     FDroid \
     FDroidPrivilegedExtension \
-    IchnaeaNlpBackend \
-    NominatimNlpBackend \
     additional_repos.xml
 
 # Privacy-friendly netmonitor
